93bd8ca6bb0130ce233c58f5a19057e726c1bc9e
eatsleepeatsleep <107299571+eatsleepeatsleep@users.noreply.github.com>
2024-11-01 21:14:19 +0800

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -72,6 +72,8 @@ def plot_truncated_normal(hospital_name, prehospital_time, lower_bound, mean, va
     plt.xlabel('The mean time from hospital arrival to definitive treatment (minutes)', fontsize=8)#, family='Times New Roman')  # 设置X轴标签字体
     plt.ylabel('Probability', fontsize=8)#, family='Times New Roman')  # 设置Y轴标签字体
     plt.legend(fontsize=8)
+    plt.show()
+    print("hi")
     
     img = io.BytesIO()
     plt.savefig(img, format='png')#, bbox_inches='tight')  # 保存图像时设置 bbox_inches='tight' 以获得更好的图像边界
