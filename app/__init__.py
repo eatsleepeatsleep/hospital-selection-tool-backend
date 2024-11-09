e4b8eedc5a80bc96724e0da977c4eff41b31c42f
eatsleepeatsleep <107299571+eatsleepeatsleep@users.noreply.github.com>
2024-11-09 21:53:13 +0800

    Update __init__.py
    更改x軸名稱/figsize(12,4)改成(8,4)

--- app/__init__.py
+++ app/__init__.py
@@ -66,14 +66,14 @@ def plot_truncated_normal(hospital_name, prehospital_time, lower_bound, mean, va
     threshold_minutes = threshold / 60
     prehospital_time_minutes = prehospital_time / 60
     
-    plt.figure(figsize=(12, 4), dpi=300)  # 提高画质，设置更高的 dpi
+    plt.figure(figsize=(8, 4), dpi=500)  # 提高画质，设置更高的 dpi
     plt.fill_between(x_minutes, 0, y, where=(x_minutes < threshold_minutes - prehospital_time_minutes) & (x_minutes >= lower_bound_minutes - prehospital_time_minutes), color='grey', alpha=0.5, label='Truncated Area: The probability of receiving definitive treatment within the threshold')
     plt.plot(x_minutes, y, color='black', label='Probability Density Function')  # 将 Density function 改为黑色的线条
     plt.axvline(threshold_minutes, color='#333', linestyle='--', label='Threshold')  
     plt.axvline(lower_bound_minutes, color='#00008B', linestyle='--', label=f'PrehospitalTime + Lower Bound: {round(lower_bound_minutes, 3):.3f} minutes')  
     plt.axvline(mean_minutes, color='#007bff', linestyle='--', label=f'PrehospitalTime + Mean Time: {round(mean_minutes, 3):.3f} minutes')   
     plt.title(f'The continuous probability distribution of a patient receiving definitive treatment at {hospital_name}', fontsize=10)#, family='Times New Roman')  # 设置标题字体
-    plt.xlabel('The mean time from hospital arrival to definitive treatment (minutes)', fontsize=8)#, family='Times New Roman')  # 设置X轴标签字体
+    plt.xlabel('The expected time for a patient from onset to receiving in hospital h', fontsize=8)#, family='Times New Roman')  # 设置X轴标签字体
     plt.ylabel('Probability', fontsize=8)#, family='Times New Roman')  # 设置Y轴标签字体
     plt.legend(fontsize=8)
     
